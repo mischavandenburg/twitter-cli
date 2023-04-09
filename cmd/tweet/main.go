de0402f2b843cf99d754ec0500a2f07f685722fd
Mischa van den Burg <mischavandenburg@gmail.com>
2023-04-09 11:59:23 +0200

    posting hardcoded tweet with tweet command

--- cmd/tweet/main.go
+++ cmd/tweet/main.go
@@ -1,8 +1,9 @@
-// Tweet handles input from the command line
-package tweet
+package main
 
 import "github.com/mischavandenburg/twitter-cli"
 
+var text string = "Hello World! Testing a tweet using my cli command.\n#go #coding #study #learning"
+
 func main() {
-	twitter.Post()
+	twitter.Post(text)
 }
--- twitter.go
+++ twitter.go
@@ -1,7 +1,38 @@
 package twitter
 
-import "fmt"
+import (
+	"context"
+	"log"
+	"os"
 
-func Post() {
-	fmt.Println("Hello World")
+	"github.com/michimani/gotwi"
+	"github.com/michimani/gotwi/tweet/managetweet"
+	"github.com/michimani/gotwi/tweet/managetweet/types"
+)
+
+func Post(text string) error {
+	in := &gotwi.NewClientInput{
+		AuthenticationMethod: gotwi.AuthenMethodOAuth1UserContext,
+		OAuthToken:           os.Getenv("TWITTER_ACCESS_TOKEN"),
+		OAuthTokenSecret:     os.Getenv("TWITTER_ACCESS_TOKEN_SECRET"),
+	}
+
+	c, err := gotwi.NewClient(in)
+	if err != nil {
+		log.Println(err)
+		return err
+	}
+
+	p := &types.CreateInput{
+		Text: gotwi.String(text),
+	}
+
+	res, err := managetweet.Create(context.Background(), c, p)
+	if err != nil {
+		log.Println(err.Error())
+		return err
+	}
+
+	log.Printf("[%s] %s\n", gotwi.StringValue(res.Data.ID), gotwi.StringValue(res.Data.Text))
+	return nil
 }
