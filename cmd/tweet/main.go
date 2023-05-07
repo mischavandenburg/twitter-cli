ecbafa9cbc2dc938e0e8c9fd508616c49a2d24f8
Mischa van den Burg <mischavandenburg@gmail.com>
2023-05-07 08:30:17 +0200

    read arguments and formulate tweet from that

--- cmd/tweet/main.go
+++ cmd/tweet/main.go
@@ -1,21 +1,46 @@
 package main
 
 import (
+	"fmt"
 	"os"
 	"strings"
 
-	"github.com/mischavandenburg/twitter-cli"
 	"github.com/mischavandenburg/twitter-cli/internal/feed"
 )
 
 var tags string
+var action string
+var tweet string
 
 func main() {
 	if len(os.Args) > 1 {
-		tags = "\n" + strings.Join(os.Args[1:], " ")
+		action = os.Args[1]
 	}
-	r := feed.GetLatestPost()
-	tweet := r + tags
-	// fmt.Println(tweet)
-	twitter.Post(tweet)
+
+	if action == "url" {
+		url := os.Args[2]
+		tweet = url + tags
+
+		// tags have a different position if url is given
+		tags = "\n" + strings.Join(os.Args[3:], " ")
+		send()
+	}
+
+	if action == "latest" {
+		latestpost := feed.GetLatestPost()
+
+		// tags have a different position if url is given
+		tags = "\n" + strings.Join(os.Args[2:], " ")
+		tweet = latestpost + tags
+		send()
+	}
+
+	fmt.Println("Usage: tweet url url tags or tweet latest tags")
+
+}
+
+func send() {
+	fmt.Println(tweet)
+	// twitter.Post(tweet)
+	os.Exit(0)
 }
